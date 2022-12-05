59a40437f95ce8b4e2610aa23d6eb31c2e759375
nangcr <nangcr@gmail.com>
2022-12-05 12:09:23 +0800

    modify

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-wget https://s1.cloud.shuaninfo.com/sqldev/sqldev-free-2.2.60-202211090526.tar.gz
+cd /opt
+wget http://s1.cloud.shuaninfo.com/sqldev/sqldev-free-2.2.60-202211090526.tar.gz
 tar -zxvf sqldev-free-2.2.60-202211090526.tar.gz
 cd sqldev
 sh install.sh
